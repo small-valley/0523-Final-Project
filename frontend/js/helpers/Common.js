292096571799406b3e6f1f4fe58ca9a987c512a5
small-valley <kuzan1123@gmail.com>
2023-08-24 16:45:39 -0700

    add api request functions

--- frontend/js/helpers/Common.js
+++ frontend/js/helpers/Common.js
@@ -1,1 +1,25 @@
 //common functions that can be used in different cases
+const BASE_URL = "http://localhost:3000";
+
+function Get(url) {
+    return ApiRequest(url, "GET");
+}
+
+function Post(url, body) {
+    return ApiRequest(url, "POST", body);
+}
+
+function ApiRequest(url, type, body) {
+    $.ajax({
+        url: url,
+        type: type,
+        data: JSON.stringify(body),
+        contentType: "application/json",
+        success: (data) => {
+            return data;
+        },
+        error: (err) => {
+            console.log(err);
+        },
+    });
+}
