8af725df8a30fec55f9de9aa1f4e31d248cdd512
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-08-28 16:39:38 -0700

    add new account with balance 0

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -1,10 +1,71 @@
-function addAccount(event) {
-  event.preventDefault();   
-  const accountName = $('#accountInput').val();
-  const tableRow = $('<tr></tr>');
-  const accountCell = $('<td></td>').text(accountName);
-  let balanceCell = $('<td></td>').text(0);
-  tableRow.append(accountCell);
-  tableRow.append(balanceCell);
-  $('#summary').append(tableRow);
-}
+let accId = undefined;
+
+let accountBalance = 0;
+
+
+$(document).ready(() => {
+
+  $("#addAcc").submit((event) => {
+    event.preventDefault();
+    const newName = $("#accountInput").val().trim();
+
+    const accountExists = $("#accName td").toArray().some(td => td.textContent === newName);
+    if (accountExists) {
+      alert("Account already exists!");
+      $("#accountInput").val("");
+      return;
+    }
+
+    $.ajax({
+      method: "post",
+      data: {
+        newAccount: newName,
+      },
+      url: "http://localhost:3000/accounts",
+      dataType: "json",
+    }).done((data) => {
+      $.ajax({
+        method: "get",
+        url: "http://localhost:3000/accounts",
+        dataType: "json",
+      }).done((accData) => {
+        let accountName = $("#accountInput").val();
+        console.log("accData", accData);
+        if (accData.slice(-1)[0]) {
+          accId = accData.slice(-1)[0].id;
+          console.log("accId", accId);
+          $("#accName").append(() =>{
+            if($(`accName td[value="${accId}"]`).length == 0){
+              return $(
+                `<tr>
+                <td value="${accId}">${accountName}</td>
+                </tr>`
+              );
+            }
+          });
+          $("#accountInput").val("");
+          $("#accBalance").append(`
+            <tr>
+              <td value="${accId}">${accountBalance}</td>
+            </tr>
+          `);
+          
+          $("#account").append(`
+            <option value="${accId}">${accountName}</option>
+          `);
+          $("#from").append(`
+            <option value="${accId}">${accountName}</option>
+          `);
+          $("#to").append(`
+            <option value="${accId}">${accountName}</option>
+          `);
+          $("#FilterByAccount").append(`
+            <option value="${accId}">${accountName}</option>
+          `);
+        };
+        
+      });
+    });
+  });
+});
+
