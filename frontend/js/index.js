761dc5f39ef2fb39d5513e759fefb24256c4e61b
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-08-30 14:28:27 -0700

    fixed comments

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,26 @@
 $(() => {
   //Start coding here!
 });
+const typeDeposit = document.getElementById("type-deposit");
+  const typeWithdraw = document.getElementById("type-withdraw");
+  const typeTransfer = document.getElementById("type-transfer");
+  const fromToContainer = document.getElementById("from-to");
+  const accountLabel = document.getElementById("acc");
+
+  // Initially hide 'From' and 'To'
+  fromToContainer.style.display = "none";
+  accountLabel.style.display = "block"; // Display 'Account' initially
+
+  // Add event listeners to radio buttons
+  typeDeposit.addEventListener("change", toggleFromTo);
+  typeWithdraw.addEventListener("change", toggleFromTo);
+  typeTransfer.addEventListener("change", toggleFromTo);
+
+  function toggleFromTo() {
+    // Show 'From' and 'To' only for 'Transfer'
+    fromToContainer.style.display =
+      typeTransfer.checked ? "block" : "none";
+    
+    // Hide 'Account' label when 'Transfer' is selected
+    accountLabel.style.display = typeTransfer.checked ? "none" : "block";
+  }
