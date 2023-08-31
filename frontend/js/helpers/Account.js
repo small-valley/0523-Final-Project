ca0627c00e69a1be00fb8ec2798d26696734d7ab
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-08-31 15:58:28 -0700

    add clc balance functions to Account.js

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -1,29 +1,66 @@
 $(document).ready(async() => {
-  function updateUI(account) {
+  //1. update UI func
+  function updateUI(account,balance=0) {
     $("#accName").append(
       `<tr>
-        <td>${account.name}</td>
-        <td>0</td>
+        <td>${account.username}</td>
+        <td>${balance}</td>
         <td><button class="removeBtn" data-account-id="${account.id}">Remove Account</button></td>
       </tr>`
     );
     $("#account, #from, #to, #FilterByAccount,#accountId,#accountIdTo,#accountIdFrom").append(
-      `<option value="${account.id}">${account.name}</option>`
+      `<option value="${account.id}">${account.username}</option>`
     );
   }
-async function fetchAccounts(){
-  const result = await Get("accounts");
-  return result;
-}
-await fetchAccounts((account) => {
-    updateUI(account);
-  });
 
+  //2. calc Balance func
+
+  function calcBalance(accountTransactions,balance=0){
+    if(accountTransactions){
+      accountTransactions.forEach(transaction => {
+        if (transaction.type === "Deposit") {
+          balance += parseInt(transaction.amount);
+        } else if (transaction.type === "Withdraw") {
+          balance -= parseInt(transaction.amount);
+        } else if (transaction.type === "Transfer") {
+          if (transaction.accountId === transaction.accountIdFrom) {
+            balance -= parseInt(transaction.amount);
+          } else if (transaction.accountId === transaction.accountIdTo) {
+            balance += parseInt(transaction.amount);
+          } else {
+            console.log("Unknown type:", transaction);
+          }
+        } else {
+          console.log("Unknown type:", transaction);
+        }
+      });
+    }
+    return balance;
+  }
+
+
+  //3. edit page loading process 
+  async function onPageLoad() {
+    const transactions = await Get("transactions");
+    const accounts = await Get("accounts");
+    for (const account of accounts) {
+      // Extract only elements matching the specified accountId
+      const matchingTransactionsArray = transactions.find(accountArray => {
+        return accountArray.some(transaction => transaction.accountId === account.id);
+      });
+      // calc balance
+      balance=calcBalance(matchingTransactionsArray)
+
+      updateUI(account, balance);
+    }
+  }
+  onPageLoad(); 
+
+
+  //4. when addAcc clicked process
   $("#addAcc").submit((event) => {
     event.preventDefault();
     const newName = $("#accountInput").val().trim();
-
-
     $.ajax({
       method: "post",
       data: {
@@ -32,7 +69,7 @@ await fetchAccounts((account) => {
       url: "http://localhost:3000/accounts",
       dataType: "json",
     }).done((data) => {
-      const newAccount = { id: data.id, name: newName, balance: 0 };
+      const newAccount = { id: data.id, username: newName};
       // Update the UI
       updateUI(newAccount);
 
