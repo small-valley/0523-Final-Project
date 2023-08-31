59ee1c02715a4a6df66433665e6a4e7ccee3eb8c
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-08-30 17:08:28 -0700

    fixed comments

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -1,11 +1,4 @@
 $(document).ready(async() => {
-  // Load accounts from local storage on page load
-  // let savedAccounts = JSON.parse(localStorage.getItem("accounts")) || [];
-
-  // Filter out any undefined accounts
-  // savedAccounts = savedAccounts.filter(account => account && account.id && account.name && account.balance !== undefined);
-
-  // Update the UI with account details
   function updateUI(account) {
     $("#accName").append(
       `<tr>
@@ -26,32 +19,10 @@ await fetchAccounts((account) => {
     updateUI(account);
   });
 
-  // Handle the remove account button click event
-  // $("#accName").on("click", ".removeBtn", function() {
-  //   const accountId = $(this).data("account-id");
-  //   savedAccounts = savedAccounts.filter(account => account.id !== accountId);
-  //   localStorage.setItem("accounts", JSON.stringify(savedAccounts));
-  //   $(this).closest("tr").remove();
-  // });
-
   $("#addAcc").submit((event) => {
     event.preventDefault();
     const newName = $("#accountInput").val().trim();
 
-    // const accountExists = savedAccounts.some(account => account.name === newName);
-    // if (accountExists) {
-    //   // Show the modal
-    //   $("#myModal").css("display", "block");
-    //   $("#accountInput").val(""); // Clear input
-  
-    //   // Close the modal when the close button is clicked
-    //   $(".close").click(function() {
-    //     $("#myModal").css("display", "none"); // Hide the modal
-    //   });
-  
-    //   return;
-    // }
-  
 
     $.ajax({
       method: "post",
@@ -61,11 +32,7 @@ await fetchAccounts((account) => {
       url: "http://localhost:3000/accounts",
       dataType: "json",
     }).done((data) => {
-      // Add new account to savedAccounts
       const newAccount = { id: data.id, name: newName, balance: 0 };
-      // savedAccounts.push(newAccount);
-      // localStorage.setItem("accounts", JSON.stringify(savedAccounts));
-
       // Update the UI
       updateUI(newAccount);
 
